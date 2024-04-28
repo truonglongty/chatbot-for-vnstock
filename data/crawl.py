06b28ba41b3932503bf282f67901bcb2083a0424
truonglongty <137083025+truonglongty@users.noreply.github.com>
2024-04-28 16:21:48 +0700

    update

--- data/crawl.py
+++ data/crawl.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -16,7 +19,7 @@ browser.find_element(By.XPATH, value='//*[@id="trading-result"]/div/div[1]/div[1
 sleep(3)
 
 
-with open(r'D:\chatbot-for-stocks\scraping\login.txt', 'r') as login_file:
+with open('login.txt', 'r') as login_file:
     lines = login_file.readlines()
     mail = lines[0]
     password = lines[1]
@@ -44,3 +47,44 @@ try:
     print("Download successful")
 except:
     print("Download failed")
+
+
+download_folder = r'C:\Users\ADMIN\Downloads'
+destination_folder = r'D:\chatbot-for-stocks\data'
+
+# Biến để lưu trữ đường dẫn của file mới nhất
+newest_file_path = None
+newest_file_time = 0
+
+# Duyệt qua tất cả các file trong thư mục tải xuống
+for root, dirs, files in os.walk(download_folder):
+    for file_name in files:
+        file_path = os.path.join(root, file_name)
+        
+        # Kiểm tra nếu file là loại file bạn quan tâm (ví dụ: file Excel)
+        if file_path.endswith('.xls'):
+            try:
+                # Lấy thông tin về thời gian tạo của file
+                file_time = os.path.getctime(file_path)
+                
+                # Nếu thời gian tạo của file lớn hơn thời gian của file mới nhất hiện tại
+                if file_time > newest_file_time:
+                    newest_file_time = file_time
+                    newest_file_path = file_path
+            except Exception as e:
+                print(f"Lỗi khi kiểm tra file {file_path}: {str(e)}")
+
+
+# Kiểm tra nếu có file mới nhất được tìm thấy
+if newest_file_path:
+    try:
+        # Thực hiện di chuyển file mới nhất đến thư mục đích
+        shutil.move(newest_file_path, destination_folder)
+        print(f"Đã chuyển file {newest_file_path} đến {destination_folder}")
+    except Exception as e:
+        print(f"Lỗi khi di chuyển file {newest_file_path}: {str(e)}")
+else:
+    print("Không tìm thấy file nào để di chuyển.")
+
+
+browser.quit()
